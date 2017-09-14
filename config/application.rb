32087a22a31588be8196a94e9ec2ef888443e21c
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2017-09-13 22:38:16 -0300

    Adjusts config/application configs

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,27 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Acompanhamento
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale            = 'pt-BR'
+    config.time_zone                      = 'America/Sao_Paulo'
+    config.active_record.default_timezone = :local
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths   << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
+
