d5736bd7fa24d1fa67956c2ad1b8c31042ea1ce5
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2017-09-17 18:46:20 -0300

    Adds missing validation to Weight model spec

--- spec/models/weight_spec.rb
+++ spec/models/weight_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Weight, type: :model do
   context "Validations" do
     it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:registered_on) }
   end
 
   context "Associations" do
