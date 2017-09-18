71531f421a73db3741ee6a8dc178f8e113b6aebd
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2017-09-18 18:00:02 -0300

    Improves locales for weights and perimeters

--- spec/features/create_weight_spec.rb
+++ spec/features/create_weight_spec.rb
@@ -10,22 +10,20 @@ feature "Creating a Weight" do
       fill_in "Data de Registro", with: "21/12/2012"
       fill_in "Peso", with: "90"
 
-      click_button "Criar Peso"
+      click_button "Registrar"
 
       expect(page).to have_text("Peso registrado com sucesso")
       expect(current_path).to eql weights_path
     }.to change(Weight, :count).by(1)
   end
 
-  scenario "wigh invalid or missing data" do
+  scenario "with invalid or missing data" do
     expect {
       sign_in
 
       visit new_weight_path
 
-      fill_in "Data de Registro", with: ""
-
-      click_button "Criar Peso"
+      click_button "Registrar"
 
       expect(page).to have_text("Por favor verifique os problemas abaixo:")
       expect(page).to have_text("não pode ficar em branco")
