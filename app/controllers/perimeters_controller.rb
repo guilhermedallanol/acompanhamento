0fb9e6922b017a69b9c244f0539abacf972ce465
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2017-09-18 21:49:28 -0300

    Introduces Resourceless controller concern

--- app/controllers/perimeters_controller.rb
+++ app/controllers/perimeters_controller.rb
@@ -1,39 +1,12 @@
 class PerimetersController < ApplicationController
-  def index
-    @perimeters ||= current_user.perimeters.decorate
-  end
-
-  def new
-    @perimeter = Perimeter.new
-  end
-
-  def create
-    result = Perimeters::CreationService.perform(perimeter_params, current_user)
-
-    @perimeter = result.perimeter
-
-    if result.success?
-      redirect_to perimeters_path, notice: t(".success")
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    perimeter.destroy
-    redirect_to perimeters_path, notice: t('.success')
-  end
+  include Resourceless
 
   private
 
-  def perimeter
-    @perimeter ||= current_user.perimeters.find(params[:id])
-  end
-
-  def perimeter_params
-    params.require(:perimeter).permit(
+  def permitted_attributes
+    [
       :abdomen, :arm, :calf, :chest, :hip,
       :registered_on, :thigh, :waist, :wrist
-    )
+    ]
   end
 end
