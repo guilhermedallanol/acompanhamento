64fe5737abaf17f696d2d3ae43f99007a02ed015
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2017-09-28 23:54:46 -0300

    Removes redundant #to_s in interpolation

--- app/services/wizard_creation_base_service.rb
+++ app/services/wizard_creation_base_service.rb
@@ -3,6 +3,6 @@ class WizardCreationBaseService < CreationBaseService
   private
 
   def model
-    @model ||= @scope.public_send("build_#{model_key.to_s}", @data)
+    @model ||= @scope.public_send("build_#{model_key}", @data)
   end
 end
